8f0865c5630eb1cf6a133069f0054d18d621b19c
owaisbukhari00 <owaisbukhari@outlook.com>
2024-07-04 20:23:28 +0500

    added import file to blog container for easy importing of images

---
+++ src/containers/blog/imports.js
@@ -0,0 +1,13 @@
+import {default as blog1} from '../../assets/blog01.png';
+import {default as blog2} from '../../assets/blog02.png';
+import {default as blog3} from '../../assets/blog03.png';
+import {default as blog4} from '../../assets/blog04.png';
+import {default as blog5} from '../../assets/blog05.png';
+
+export {
+  blog1,
+  blog2,
+  blog3,
+  blog4,
+  blog5,
+}
