e18504c4c452de9f593930498562293738870902
owaisbukhari00 <owaisbukhari@outlook.com>
2024-07-05 20:23:42 +0500

    added Article component to create individual article in the blog

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import './article.css';
 
-const Article = () => {
+const Article = ( {imageURL, date, heading } ) => {
   return (
-    <div>Article</div>
+    <div className="gpt3__blog-container__article">
+      <div className="gpt3__blog-container__article-image">
+        <img src={imageURL} alt="" />
+      </div>
+
+      <div className="gpt3__blog-container__article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{heading}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   );
 }
 
